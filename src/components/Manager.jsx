6287c9ff41412569890e7cef5d946d9fe7ba3a40
Zaki <gostudyzaki@gmail.com>
2025-02-25 13:41:48 +0500

    Saving to Local Storage

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,14 +1,37 @@
-import React,{useRef} from "react";
+import React,{useRef,useState,useEffect} from "react";
 
 const Manager = () => {
-  const ref = useRef()
+  const Btnref = useRef()
+  const [form, setform] = useState({site: "", username: "", password: ""});
+  const [passArr, setpassArr] = useState([])
+
+  useEffect(() => {
+    let passwords = localStorage.getItem("passwords");
+    if(passwords){
+      setpassArr(JSON.parse(passwords))
+  }
+  }, [])
+  
+
   const showPWD = () => {
-    if(ref.current.src.includes("/icons/eye-slash-solid.svg")){
-      ref.current.src = "/icons/eye-solid.svg"
+    if(Btnref.current.src.includes("/icons/eye-slash-solid.svg")){
+      Btnref.current.src = "/icons/eye-solid.svg"
     }
     else{
-      ref.current.src = "/icons/eye-slash-solid.svg"
+      Btnref.current.src = "/icons/eye-slash-solid.svg"
     }
+  }
+
+  const handelChange = (e) => {
+    setform({ ...form, [e.target.name]: e.target.value });
+  }
+
+  const savePassword = () => {
+    console.log(form);
+    setpassArr([...passArr,form])
+    localStorage.setItem("passwords",JSON.stringify([...passArr,form]))
+    console.log([...passArr,form]);
+ 
   }
   return (
     <>
@@ -31,16 +54,18 @@ const Manager = () => {
             placeholder="Enter Website URL" 
             type="text"
             className="rounded-full border border-green-400   px-5 py-1"
-            name=""
-            id=""
+            name="site"
+            value={form.site}
+            onChange={(handelChange)}
           />
           <div className="flex gap-2">
             <input
               placeholder="Enter Username"
               type="text"
               className="border w-full border-green-400 rounded-full  px-5 py-1"
-              name=""
-              id=""
+              name="username"
+              value={form.username}
+              onChange={(handelChange)}
             />
             <div className="relative">
 
@@ -48,14 +73,15 @@ const Manager = () => {
               placeholder="Enter Password"
               type="text"
               className="border w-full border-green-400 rounded-full px-5 py-1"
-              name=""
-              id=""
+              name="password"
+              value={form.password}
+              onChange={(handelChange)}
               />
-              <span  onClick={(showPWD)} className="absolute right-3 top-2 cursor-pointer"><img ref={ref} src="/icons/eye-solid.svg" alt="" className="w-5 h-5"/></span>
+              <span  onClick={(showPWD)} className="absolute right-3 top-2 cursor-pointer"><img ref={Btnref} src="/icons/eye-solid.svg" alt="" className="w-5 h-5"/></span>
               </div>
           </div>
           <div className="btn flex justify-center items-center">
-            <button className=" bg-green-500 flex justify-center items-center border border-black rounded-full w-fit px-4 py-2 text-white gap-2 hover:bg-green-600 hover:cursor-pointer">
+            <button onClick={(savePassword)} className=" bg-green-500 flex justify-center items-center border border-black rounded-full w-fit px-4 py-2 text-white gap-2 hover:bg-green-600 hover:cursor-pointer">
               <lord-icon
                 src="https://cdn.lordicon.com/sbnjyzil.json"
                 trigger="hover"
